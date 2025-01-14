6ea9d807751cecbd2df1c48639b1be75b6e696eb
chisty2996 <chisty2996@gmail.com>
2025-01-15 01:39:29 +0600

    Route management added, feature directory created following clean, album screen added

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,12 @@
+class AppRoutes {
+  AppRoutes._();
+
+  ///Album Screen
+  static const albumScreen = "/album_screen";
+
+  ///Photos Screen
+  static const photosScreen = "/photos_screen";
+
+  ///PhotoViewer Screen
+  static const photoViewerScreen = "/photo_viewer_screen";
+}
---
+++ lib/core/routes/route_generator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:gallery_app/core/routes/app_routes.dart';
+import 'package:gallery_app/features/gallery/presentation/screens/album_screen.dart';
+import 'package:go_router/go_router.dart';
+
+class RouteGenerator{
+  
+}
+final GoRouter router = GoRouter(
+  initialLocation: AppRoutes.albumScreen,
+  routes: [
+    ///Album Screen
+    GoRoute(
+      path: AppRoutes.albumScreen,
+      pageBuilder: (context, state) => const MaterialPage(child: AlbumScreen())
+    ),
+  ],
+);
